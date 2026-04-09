1bf9f8b4d812c87e76f01f0d6618c31cf44f9886
yangnanqing <yangnq@xiaopeng.com>
2026-04-09 23:02:57 +0800

    feat: add BrowserObserver

--- src/hermes_mac/observers/__init__.py
+++ src/hermes_mac/observers/__init__.py
@@ -1,5 +1,6 @@
 """Observer subsystem."""
 
+from hermes_mac.observers.browser import BrowserObserver
 from hermes_mac.observers.base import Observer
 from hermes_mac.observers.events import ObserverEvent
 from hermes_mac.observers.registry import ObserverRegistry
@@ -7,4 +8,4 @@ from hermes_mac.observers.system import SystemObserver
 from hermes_mac.observers.vscode import VSCodeObserver
 from hermes_mac.observers.intellij import IntelliJObserver
 
-__all__ = ["Observer", "ObserverEvent", "ObserverRegistry", "SystemObserver", "VSCodeObserver", "IntelliJObserver"]
+__all__ = ["Observer", "ObserverEvent", "ObserverRegistry", "SystemObserver", "VSCodeObserver", "IntelliJObserver", "BrowserObserver"]
