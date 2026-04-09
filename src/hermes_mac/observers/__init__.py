aaee11b2c7035da15e7cb30a120a9f5ec198c97f
yangnanqing <yangnq@xiaopeng.com>
2026-04-09 22:29:16 +0800

    feat: add system observer

--- src/hermes_mac/observers/__init__.py
+++ src/hermes_mac/observers/__init__.py
@@ -3,5 +3,6 @@
 from hermes_mac.observers.base import Observer
 from hermes_mac.observers.events import ObserverEvent
 from hermes_mac.observers.registry import ObserverRegistry
+from hermes_mac.observers.system import SystemObserver
 
-__all__ = ["Observer", "ObserverEvent", "ObserverRegistry"]
+__all__ = ["Observer", "ObserverEvent", "ObserverRegistry", "SystemObserver"]
