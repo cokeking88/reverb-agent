35d97a1f62383a99aa37a6ba30141dc691eb7947
yangnanqing <yangnq@xiaopeng.com>
2026-04-09 22:27:42 +0800

    feat: add observer registry

--- src/hermes_mac/observers/__init__.py
+++ src/hermes_mac/observers/__init__.py
@@ -2,5 +2,6 @@
 
 from hermes_mac.observers.base import Observer
 from hermes_mac.observers.events import ObserverEvent
+from hermes_mac.observers.registry import ObserverRegistry
 
-__all__ = ["Observer", "ObserverEvent"]
+__all__ = ["Observer", "ObserverEvent", "ObserverRegistry"]
